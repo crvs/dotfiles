3484b2f21e63dffa87dbbf8ce7fc252591dbf96b
crvs <carvas.f@gmail.com>
2016-08-11 09:26:33 +0200

    something i did in r2d2

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -278,9 +278,9 @@ globalkeys = awful.util.table.join(
     awful.key({ }, "XF86MonBrightnessDown",   function () awful.util.spawn("xbacklight -dec 5") end),
 
     -- Volume controls // Ubuntu 14.04
-    awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn("pactl set-sink-volume 1 -- +3%") end),
-    awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn("pactl set-sink-volume 1 -- -3%") end),
-    awful.key({ }, "XF86AudioMute", function () awful.util.spawn("pactl set-sink-mute 1 -- toggle") end),
+    awful.key({ }, "XF86AudioRaiseVolume", function () awful.util.spawn("pactl set-sink-volume 0 -- +3%") end),
+    awful.key({ }, "XF86AudioLowerVolume", function () awful.util.spawn("pactl set-sink-volume 0 -- -3%") end),
+    awful.key({ }, "XF86AudioMute", function () awful.util.spawn("pactl set-sink-mute 0 -- toggle") end),
 
     -- Spotify controls
     awful.key({ }, "XF86AudioPlay", function () awful.util.spawn_with_shell("dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.PlayPause") end),
