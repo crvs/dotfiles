18ed08e8186c316be0fc875cf2a054e23a3d436e
crvs <jfpbdc@kth.se>
2018-12-17 15:25:12 +0100

    upgraded/updated (?) ros utilities loading

--- zsh/ros.zsh
+++ zsh/ros.zsh
@@ -1,23 +1,21 @@
 
 if [ -f /opt/ros/indigo/setup.zsh ]; then
-
     source /opt/ros/indigo/setup.zsh
     export ROS_HOSTNAME=localhost
     export ROS_MASTER_URI=http://localhost:11311
-
 fi
 
-if [ -d /opt/ros ]; then
-
+if [ -d /opt/ros/kinetic ]; then
     source /opt/ros/kinetic/setup.zsh
+    export ROS_HOSTNAME=localhost
+    export ROS_MASTER_URI=http://localhost:11311
+fi
 
-    if [ -d $HOME/catkin_ws ]; then
-        source $HOME/catkin_ws/devel/setup.zsh
-    fi
+if [ -d $HOME/catkin_ws/install ]; then
+    export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOME/catkin_ws/install/
+fi
 
-    if [ -d $HOME/catkin_ws/install ]; then
-        export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$HOME/catkin_ws/install/
-    fi
-
+if [ -f $HOME/catkin_ws/devel/setup.zsh ]; then
+    source $HOME/catkin_ws/devel/setup.zsh
 fi
 
