2c5eb496102410095be54a2883ed7938eb076edd
crvs <jfpbdc@kth.se>
2019-06-30 23:23:18 +0200

    added marvin hotkeys

--- java/java.zsh
+++
@@ -1,9 +0,0 @@
-export _JAVA_OPTIONS='-Dawt.useSystemAAFontSettings=on swing.aatext=true'
-export JAVA_FONTS=/usr/share/fonts/TTF
-
-# java classes
-if [ -d $HOME/.javapackages ]; then
-    for f in `find $HOME/.javapackages -name '*.jar'`
-        do export CLASSPATH=$CLASSPATH:$f
-    done
-fi
